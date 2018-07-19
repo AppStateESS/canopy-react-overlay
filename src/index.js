6993305916dbcbcaae4505b1372db617665d62c3
Matthew McNaney <mcnaney@gmail.com>
2018-07-19 09:47:59 -0400

    Added fade prop

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ export default class Overlay extends Component {
       extraStyles.position = 'absolute'
       extraStyles.top = '5%'
     }
-    
+
     if (this.props.height) {
       extraStyles.height = this.props.height
     }
@@ -49,20 +49,28 @@ export default class Overlay extends Component {
       extraStyles.overflow = this.props.overflow
     }
 
-    if (this.props.show === false) {
-      return <span></span>
+    let overlayClass =  []
+    overlayClass.push('canopy-react-overlay')
+    if (this.props.show) {
+      overlayClass.push('show')
     }
+    if (this.props.fade) {
+      overlayClass.push('fadein')
+    }
+    
     return (
-      <div className="cro-backing">
-        <div className="cro-overlay" style={extraStyles}>
-          <div className="cro-header">
-            <div className="cro-button pointer" onClick={this.close}>
-              <i className="fas fa-2x fa-times"></i>
+      <div className={overlayClass.join(' ')}>
+        <div className="cro-backing">
+          <div className="cro-overlay" style={extraStyles}>
+            <div className="cro-header">
+              <div className="cro-button pointer" onClick={this.close}>
+                <i className="fas fa-2x fa-times"></i>
+              </div>
+              <div className="cro-title">{this.props.title}</div>
             </div>
-            <div className="cro-title">{this.props.title}</div>
-          </div>
-          <div className="cro-children">
-            {this.props.children}
+            <div className="cro-children">
+              {this.props.children}
+            </div>
           </div>
         </div>
       </div>
@@ -71,6 +79,7 @@ export default class Overlay extends Component {
 }
 
 Overlay.propTypes = {
+  fade: PropTypes.bool,
   children: PropTypes.node,
   close: PropTypes.func.isRequired,
   title: PropTypes.string,
@@ -78,4 +87,9 @@ Overlay.propTypes = {
   height: PropTypes.string,
   show: PropTypes.bool,
   overflow: PropTypes.string
+}
+
+Overlay.defaultProps = {
+  fade : false,
+  show: false,
 }
