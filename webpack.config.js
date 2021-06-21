03e8f4ee984c1cc2a341e26ff5ee9d373c376b93
Matt McNaney <mcnaneym@appstate.edu>
2021-06-21 12:34:47 -0400

    Updated package and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,46 @@
 /* global __dirname, module */
 const path = require('path')
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
-  },
-  resolve: {
-    alias: {
-      react: path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+module.exports = (env, argv) => {
+  const inDevelopment = argv.mode === 'development'
+
+  const settings = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.js',
+      libraryTarget: 'commonjs2',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        include: path.resolve(__dirname, 'src'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
+    resolve: {
+      alias: {
+        react: path.resolve(__dirname, './node_modules/react'),
+        'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
       },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.jsx?/,
+          resolve: {
+            extensions: ['.js', '.jsx'],
+          },
+          include: path.resolve(__dirname, 'src'),
+          use: {
+            loader: 'babel-loader',
             options: {
-              implementation: require('dart-sass'),
+              presets: ['@babel/preset-env', '@babel/preset-react'],
             },
           },
-        ],
-      },
-    ],
-  },
+        },
+      ],
+    },
+  }
+
+  if (inDevelopment) {
+    settings.devtool = 'inline-source-map'
+  }
+  return settings
 }
